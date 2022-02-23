35ee8fa527be790cb519c26460c975b1fd69ed96
KTangg <k.tangtangtang@gmail.com>
2022-02-23 21:50:05 +0700

    handle - cases

--- srcs/extend_format_util_1.c
+++ srcs/extend_format_util_1.c
@@ -24,27 +24,57 @@ static void	filling(void *var, size_t n, char c)
 	}
 }
 
-int	extend_zero(void **var, size_t *var_size, char **var_format, size_t org)
+static void	*before_signed(size_t min, size_t org, size_t var_size, void *var)
 {
 	size_t			fill;
 	size_t			extend;
+	unsigned char	*new;
+
+	extend = var_size - org;
+	new = malloc(min);
+	if (!new)
+		return (0);
+	ft_memcpy(new, var, extend);
+	filling(&new[extend], fill, '0');
+	ft_memcpy(&new[extend + fill], &(((unsigned char *)var)[extend]), org);
+	return ((void *)new);
+}
+
+static void	*after_signed(size_t min, size_t org, size_t var_size, void *var)
+{
+	size_t			fill;
+	size_t			extend;
+	unsigned char	*new;
+
+	extend = var_size - org + 1;
+	new = malloc(min);
+	if (!new)
+		return (0);
+	ft_memcpy(new, var, extend);
+	filling(&new[extend], fill, '0');
+	ft_memcpy(&new[extend + fill], &(((unsigned char *)var)[extend]), org - 1);
+	return ((void *)new);
+}
+
+int	extend_zero(void **var, size_t *var_size, char **var_format, size_t org)
+{
+	void			*new_var;
 	size_t			min;
-	unsigned char	*new_var;
+	unsigned char	c;
 
 	*var_format = *var_format + 1;
-	min = ft_atoi(*var_format);
-	extend = *var_size - org;
+	min = (size_t)ft_atoi(*var_format);
+	c = ((unsigned char *)*var)[0];
 	if (min > *var_size)
 	{
-		fill = min - *var_size;
-		new_var = malloc(min);
-		if (!new_var)
-			return (0);
-		ft_memcpy(new_var, *var, *var_size - org);
-		filling(&new_var[extend], fill, '0');
-		ft_memcpy(&new_var[extend + fill], &(((unsigned char *)*var)[extend]), org);
+		if (c == '-')
+			new_var = after_signed(min, org, *var_size, *var);
+		else
+			new_var = before_signed(min, org, *var_size, *var);
 		free(*var);
 		*var_size = min;
+		if (!new_var)
+			return (0);
 		*var = new_var;
 	}
 	return (1);
@@ -65,7 +95,10 @@ int	extend_min(void **var, size_t *var_size, char **var_format, size_t org)
 		fill = min - *var_size;
 		new_var = malloc(min);
 		if (!new_var)
+		{
+			free(var);
 			return (0);
+		}
 		ft_memcpy(new_var, *var, *var_size - org);
 		filling(&new_var[extend], fill, ' ');
 		ft_memcpy(&new_var[extend + fill], &(((unsigned char *)*var)[extend]), org);
