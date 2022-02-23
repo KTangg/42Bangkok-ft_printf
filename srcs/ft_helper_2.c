43ea56b2ea8bdb44ff200c11bf0cf1ec3efaf903
KTangg <k.tangtangtang@gmail.com>
2022-02-23 20:59:30 +0700

    added ft_atoi f(x)

--- srcs/ft_helper_2.c
+++ srcs/ft_helper_2.c
@@ -55,3 +55,35 @@ char	*ft_itoa(long n)
 	str[size] = '\0';
 	return (str);
 }
+
+static int	ft_isspace(char c)
+{
+	if ((c >= 9 && c <= 13) || c == 32)
+		return (1);
+	return (0);
+}
+
+int	ft_atoi(const char *nptr)
+{
+	long int	result;
+	long int	negative;
+
+	result = 0;
+	negative = 1;
+	while (ft_isspace(*nptr))
+		nptr++;
+	if (*nptr == '+' || *nptr == '-')
+	{
+		if (*nptr == '-')
+			negative = -1;
+		nptr++;
+	}
+	while (ft_isdigit(*nptr))
+	{
+		result = result * 10;
+		result = result + (*nptr - '0');
+		nptr++;
+	}
+	result = result * negative;
+	return (result);
+}
