e3dfdd6c22c7303b2eccf2abbfaff6c80e4d642e
KTangg <k.tangtangtang@gmail.com>
2022-02-28 23:52:31 +0700

    extend dot reworked

--- srcs/extend_format_util_4.c
+++ srcs/extend_format_util_4.c
@@ -27,7 +27,7 @@ static void	filling(void *var, size_t n, char c)
 
 size_t	find_start(void *var, size_t *var_size, size_t *org)
 {
-	size_t			*i;
+	size_t			i;
 	size_t			org_size;
 	unsigned char	*str;
 
@@ -47,19 +47,23 @@ size_t	find_start(void *var, size_t *var_size, size_t *org)
 
 int	extend_dot(void **var, size_t *var_size, char **var_format, size_t org)
 {
-	void			*new;
 	size_t			min;
+	size_t			fill;
 	size_t			start;
-	unsigned char	*c;
+	unsigned char	*new;
 
 	*var_format = *var_format + 1;
 	min = (size_t)ft_atoi(*var_format);
 	start = find_start(*var, var_size, &org);
 	if (min > org)
 	{
-		new = malloc(min - org + *var_size);
+		fill = min - org;
+		new = (unsigned char *)malloc(fill + *var_size);
+		ft_memcpy(new, *var, *var_size - org);
+		filling(&new[*var_size - org], fill, '0');
+		ft_memcpy(&new[*var_size - org + fill], (*var + *var_size - org), org);
 		free(*var);
-		*var_size = *var_size + (min - org);
+		*var_size = *var_size + fill;
 		if (!new)
 			return (0);
 		*var = new;
